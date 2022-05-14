49a7ee67954a6e408753d6c162eea323d319c811
Thomas <thomasfindlay94@gmail.com>
2022-05-14 13:56:00 +0100

    gitignore and center table

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import { tableOptions } from './tableOptions';
-import Table from './components/table/Table';
+import { useState } from "react";
+import "./App.css";
+import { tableOptions } from "./tableOptions";
+import Table from "./components/table/Table";
 
 function App() {
-  const onViewData = (column) => {
+  const onViewData = column => {
     alert(`On View Column ${column.column}`);
   };
-  const onEditData = (column) => {
+  const onEditData = column => {
     alert(`On Edit Column ${column.column}`);
   };
-  const onDeleteData = (column) => {
+  const onDeleteData = column => {
     alert(`On Delete Column ${column.column}`);
   };
 
@@ -19,7 +19,7 @@ function App() {
       <h1 className="text-center font-semibold text-lg">
         React Enterprise Component Patterns - JSX Injection via Context API
       </h1>
-      <main className="mt-8">
+      <main className="mt-8 flex justify-center">
         <Table
           options={tableOptions}
           onViewData={onViewData}
